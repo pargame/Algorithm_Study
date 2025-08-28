6b10647937adf5a5fb615f04b35d2387556b34d9
pargame <117472201+pargame@users.noreply.github.com>
2025-08-28 16:14:49 +0900

    feat: add BFS solution for 게임맵최단거리

---
+++ 게임맵최단거리.cpp
@@ -0,0 +1,34 @@
+#include<vector>
+#include<utility>
+#include<queue>
+
+using namespace std;
+
+vector<pair<int,int>> dir={{1,0},{-1,0},{0,1},{0,-1}};
+
+int solution(vector<vector<int> > maps)
+{
+    //init
+    queue<pair<int,int>> locQ;
+    locQ.push({0,0});
+    queue<int> stepQ;
+    stepQ.push(1);
+
+    //BFS
+    while(!locQ.empty()){
+        pair<int,int> cur_loc=locQ.front();
+        int cur_step=stepQ.front();
+        locQ.pop();
+        stepQ.pop();
+        if(cur_loc.first==maps.size()-1 && cur_loc.second==maps[0].size()-1) return cur_step;
+
+        for(auto [di,dj]:dir){
+            int ni=cur_loc.first+di;
+            int nj=cur_loc.second+dj;
+            if(ni<0 || ni>=maps.size() || nj<0 || nj>=maps[0].size()||maps[ni][nj]==0) continue;
+            locQ.push({ni,nj});
+            stepQ.push(cur_step+1);
+        }
+    }
+    return -1;
+}
