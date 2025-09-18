545e4dcfc0e1edd801a8a21311efef34da3a967a
pargame <117472201+pargame@users.noreply.github.com>
2025-09-19 00:32:22 +0900

    feat: implement iterative pathfinding algorithm and add VS Code configuration files

--- 빵집.cpp
+++ 빵집.cpp
@@ -1,73 +1,69 @@
 // https://www.acmicpc.net/problem/3109
+#include <algorithm>
 #include <iostream>
-#include <utility>
+#include <queue>
+#include <stack>
 #include <vector>
 
 using namespace std;
 
-int constexpr NONE = 2;
+int R, C;
+vector<vector<char>> grid;
+vector<vector<bool>> visited;
 
-class Element {
-  public:
-    vector<vector<int>> waitQ;
-    Element() = default;
-    Element(vector<vector<char>> map, int const _i) {
-        int i = _i, j = 0; // 최근 업데이트 된 map 좌표
-        vector<pair<int, vector<int>>> S(
-            1, {NONE, GetPsbs(map, i, j)}); // 기저 요소
-        while (!S.empty()) {
-            if (S.size() == map[0].size()) { // 모든 열 전파된 상황을 찾았으면
-                waitQ.push_back(vector<int>()); // 새로 벡터 만들고
-                for (auto [cur, _] : S)         // 그곳에 cur 모두 푸시
-                    waitQ.back().push_back(cur);
-                S.pop_back(); // pop
-                continue;
-            }
-            pair<int, vector<int>> top = S.back();
-            if (top.second.empty()) { // top의 가능 후보(psbs)가 더 없으면
-                map[i][j] = '.';      // map 역전파
-                i -= top.first, --j;  // i, j 역전파
-                S.pop_back();         // pop
-                continue;
-            }
-            // 푸시 상황
-            int cur = top.second.back();
-            i += cur, ++j; // i,j 업데이트
-            top.second.pop_back();
-            map[i][j] = 'X';
-            S.push_back({cur, GetPsbs(map, i, j)});
-        }
+bool find_path_iterative(int start_x) {
+  struct Frame
+  {
+    int x;
+    int y;
+    int next;
+  };
+  int dirs[3] = { -1, 0, 1 };
+  stack<Frame> st;
+  st.push({ start_x, 0, 0 });
+  visited[start_x][0] = true;
+
+  while (!st.empty()) {
+    Frame& f = st.top();
+    int x = f.x;
+    int y = f.y;
+    if (y == C - 1)
+      return true;
+    if (f.next < 3) {
+      int dx = dirs[f.next++];
+      int nx = x + dx;
+      int ny = y + 1;
+      if (nx >= 0 && nx < R && ny < C && grid[nx][ny] == '.' &&
+        !visited[nx][ny]) {
+        visited[nx][ny] = true; // mark when pushing (match recursive behavior)
+        st.push({ nx, ny, 0 });
+      }
     }
+    else st.pop();
 
-    vector<int> GetPsbs(vector<vector<char>> const &_map, int const &_i, int j) {
-        ++j;
-        if (_map[0].size() == j)
-            return vector<int>();
-
-        vector<int> rtn;
-        for (int i : {_i - 1, _i, _i + 1}) {
-            if (i < 0 || _map.size() <= i || _map[i][j] == 'X')
-                continue;
-            rtn.push_back(i - _i);
-        }
-        return rtn;
-    }
-};
+  }
+  return false;
+}
 
 int main() {
-    int R = 0, C = 0;
-    cin >> R >> C;
-    vector<vector<char>> map(R, vector<char>(C));
-    for (vector<char> &_ : map)
-        for (char &__ : _)
-            cin >> __;
-
-    Element test(map, 0);
-    for (vector<int> &Q : test.waitQ) {
-        for (int &n : Q)
-            cout << n << ' ';
-        cout << endl;
+  cin >> R >> C;
+  grid.assign(R, vector<char>(C));
+  for (int i = 0; i < R; ++i) {
+    for (int j = 0; j < C; ++j) {
+      cin >> grid[i][j];
     }
+  }
 
-    return 0;
+  visited.assign(R, vector<bool>(C, false));
+  int count = 0;
+
+  // 위쪽 행부터 시작해서 그리디하게 파이프 설치
+  for (int i = 0; i < R; ++i) {
+    if (grid[i][0] == '.' && !visited[i][0] && find_path_iterative(i)) {
+      count++;
+    }
+  }
+
+  cout << count;
+  return 0;
 }
