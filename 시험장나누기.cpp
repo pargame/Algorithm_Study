49aa5d1836c6ead2586576c06c95614a0ac9d2fb
pargame <117472201+pargame@users.noreply.github.com>
2025-09-24 17:52:26 +0900

    feat: update VS Code settings and refactor 시험장 나누기 solution for improved structure and clarity

--- 시험장나누기.cpp
+++ 시험장나누기.cpp
@@ -1,125 +1,33 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/81305
 #include <vector>
-#include<algorithm>
-#include<numeric>
-#include<queue>
+#include<utility>
 #include<stack>
-//#include<iostream>
-//#include<format>
+#include<algorithm>
 
 using namespace std;
 
-vector<int> nums;
+int k, root_idx;
+vector<int> sums;
 vector<vector<int>> links;
-int root_idx;
-int k;
+vector<int> prnts;
 
-int GetTotal(int idx, vector<vector<int>> const &links) {
-    if(idx == -1) return 0;
-    queue<int> q; q.push(idx);
-    int total = 0;
-    while(!q.empty()) {
-        total += ::nums[q.front()];
-        if(links[q.front()][0] != -1) q.push(links[q.front()][0]);
-        if(links[q.front()][1] != -1) q.push(links[q.front()][1]);
-        q.pop();
+int GetRookIdx_Prnts(vector<int>const &nums) {
+    prnts.assign(nums.size(), -1);
+    for(int i = 0; i < nums.size(); ++i) {
+        if(links[i][0] != -1) prnts[links[i][0]] = i;
+        if(links[i][1] != -1) prnts[links[i][1]] = i;
     }
-    return total;
+    root_idx = find(prnts.begin(), prnts.end(), -1) - prnts.begin();
 }
 
-bool isPsb(int M) {
-    vector<vector<int>> links = ::links;
-    queue<int> q;
-    stack<int> st;
-    //BFS로 stack에 추가->리프노드부터 팝 될 예정
-    q.push(root_idx);
-    while(!q.empty()) {
-        st.push(q.front());
-        if(links[q.front()][0] != -1) q.push(links[q.front()][0]);
-        if(links[q.front()][1] != -1) q.push(links[q.front()][1]);
-        q.pop();
-    }
-    int cur_k = 1;
-    while(!st.empty()) {
-        //cout << format("st.top:{}\n", st.top());
-        //cout << format("lidx: {},    ridx: {}\n", links[st.top()][0], links[st.top()][1]);
-
-
-        int left_total = GetTotal(links[st.top()][0], links);
-        int right_total = GetTotal(links[st.top()][1], links);
-        //cout << format("Left_total:{}    R_total:{}\n", left_total, right_total);
-
-        //불가능 할 때
-        if(::nums[st.top()] + left_total + right_total > M) {
-            //그룹 수가 오버하면 바로 false 리턴
-            //cout << format("k:{}  cur_k:{}", k, cur_k);
-            if(cur_k == k) return false;
-            //그룹 수 증가
-            ++cur_k;
-            //자식 중 서브트리 합이 큰 자식과 연결을 끊음
-            left_total < right_total ? links[st.top()][1] = -1 : links[st.top()][0] = -1;
-            continue;
-        }
-        st.pop();
-    }
-    return true;
+void MakeSumTree(vector<int>const &nums) {
+    sums.assign(nums.begin(), nums.end());
+    stack<int> st; st.push(root_idx);
 }
 
 int solution(int k, vector<int> nums, vector<vector<int>> links) {
-    ::nums = std::move(nums);
-    ::links = std::move(links);
-    vector<bool>vst(::nums.size());
-    for(int i = 0; i < vst.size(); ++i) {
-        if(::links[i][0] != -1) vst[::links[i][0]] = true;
-        if(::links[i][1] != -1) vst[::links[i][1]] = true;
-    }
-    ::root_idx = find(vst.begin(), vst.end(), false) - vst.begin();
-    ::k = k;
+    ::links = links;
+    SetRootIdx_Prnts(nums);
+    MakeSumTree(nums);
 
-    int L = *max_element(::nums.begin(), ::nums.end());
-    int R = accumulate(::nums.begin(), ::nums.end(), 0);
-    int ans = R;
-    while(L <= R) {
-        int M = (L + R) / 2;
-        //cout << format("\n==========M:{}===========\n", M);
-        if(isPsb(M)) {
-            ans = M;
-            R = M - 1;
-        }
-        else L = M + 1;
-    }
-
-    return ans;
 }
-
-// int main() {
-//     // 테스트 1
-//     {
-//         int k = 3;
-//         vector<int> nums = { 12, 30, 1, 8, 8, 6, 20, 7, 5, 10, 4, 1 };
-//         vector<vector<int>> links = { {-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}, {8, 5}, {2, 10}, {3, 0}, {6, 1}, {11, -1}, {7, 4}, {-1, -1}, {-1, -1} };
-//         cout << "Test 1: \n" << solution(k, nums, links) << " (****************expected: 40)" << endl;
-//     }
-//     // 테스트 2
-//     {
-//         int k = 1;
-//         vector<int> nums = { 6, 9, 7, 5 };
-//         vector<vector<int>> links = { {-1, -1}, {-1, -1}, {-1, 0}, {2, 1} };
-//         cout << "Test 2: \n" << solution(k, nums, links) << " (*****************expected: 27)" << endl;
-//     }
-//     //테스트 3
-//     {
-//         int k = 2;
-//         vector<int> nums = { 6, 9, 7, 5 };
-//         vector<vector<int>> links = { {-1, -1}, {-1, -1}, {-1, 0}, {2, 1} };
-//         cout << "\n\n\nTest 3: \n" << solution(k, nums, links) << " (**************expected: 14)" << endl;
-//     }
-//     // 테스트 4
-//     {
-//         int k = 4;
-//         vector<int> nums = { 6, 9, 7, 5 };
-//         vector<vector<int>> links = { {-1, -1}, {-1, -1}, {-1, 0}, {2, 1} };
-//         cout << "Test 4: \n" << solution(k, nums, links) << "******************** (expected: 9)" << endl;
-//     }
-//     return 0;
-// }
