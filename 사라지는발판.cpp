72847214c454852aab60bc9fc239d21c52963a5d
pargame <001201parg@gmail.com>
2025-12-13 22:01:47 +0900

    Refactor code structure for improved readability and maintainability

--- 사라지는발판.cpp
+++ 사라지는발판.cpp
@@ -1,56 +1,154 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/92345
-// 제한사항
-// 1 ≤ board의 세로 길이 ≤ 5
-// 1 ≤ board의 가로 길이 ≤ 5
-// board의 원소는 0 또는 1입니다.
-// 0은 발판이 없음을, 1은 발판이 있음을 나타냅니다.
-// 게임 보드의 좌측 상단 좌표는 (0, 0), 우측 하단 좌표는 (board의 세로 길이 - 1, board의 가로 길이 - 1)입니다.
-// aloc과 bloc은 각각 플레이어 A의 캐릭터와 플레이어 B의 캐릭터 초기 위치를 나타내는 좌표값이며 [r, c] 형태입니다.
-// r은 몇 번째 행인지를 나타냅니다.
-// 0 ≤ r < board의 세로 길이
-// c는 몇 번째 열인지를 나타냅니다.
-// 0 ≤ c < board의 가로 길이
-// 초기 보드의 aloc과 bloc 위치는 항상 발판이 있는 곳입니다.
-// aloc과 bloc이 같을 수 있습니다.
-// 상대 플레이어의 캐릭터가 있는 칸으로 이동할 수 있습니다.
 #include <vector>
-#include <utility>
 #include <stack>
 #include <utility>
 #include <algorithm>
 
 using namespace std;
 
-struct Frame {
-    int i, j;
-    int v = -1;  //최적값
-    int nd = 0;  //다음 방향
-    Frame(int _i, int _j) :i(_i), j(_j) {}
-};
+class Solver{
+    struct Loc{
+        int x;
+        int y;
+        Loc(int _x, int _y):x(_x),y(_y){}
+    };
+    struct Frame{
+        int nd=0;
+        Loc loc;
+        vector<int> wc;
+        vector<int> lc;
+        Frame(Loc _loc):loc(_loc){}
+    };
 
-vector<pair<int, int>> dir{ {1,0} ,{0,1} ,{-1,0} ,{0,-1} };
+    vector<stack<Frame>> st;
+    vector<vector<int>> board;
+    vector<pair<int,int>> dir{{1,0},{0,1},{-1,0},{0,-1}};
 
-int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) {
-    stack<Frame> st1, st2;
-    st1.push(Frame(aloc[0], aloc[1]));
-    st2.push(Frame(bloc[0], bloc[1]));
-
-    while(!(st1.size() == 1 && st1.top().nd == 4)) {
-        stack<Frame> &turn = (st1.size() + st2.size()) % 2 == 0 ? st1 : st2;
-        Frame &top = turn.top();
-
-        int ni = top.i + dir[top.nd].first;
-        int nj = top.j + dir[top.nd].second;
-        if(ni < 0 || nj < 0 || board.size() <= ni || board[0].size() <= nj || board[ni][nj] == 0) {
-            ++top.nd;
+    stack<Frame>& GetS(){
+        if(st[0].size()==st[1].size()){
+            return st[0];
         }
-        else if() {
-
+        else{
+            return st[1];
         }
-
-
-
     }
 
+    Frame& GetP(){
+        return GetS().top();
+    }
 
+    bool Update(){
+        //[..OPP-P-OP] 이 상태라고 해보면 P차례임(아직 안 감. 갈 곳이 더이상 없음)
+        //1. P가 이기거나 진 모두의 경우가 없다면, P는 지금 갈 곳이 하나도 없이 진 것임.
+        //      (따라서 P는 현재 걸음수가 유일한 진 값이 됨)
+        //      =반대로 OPP는 wc에 그 값을 추가하면 됨
+        //2. P가 이긴 경우만 있다면, P는 이긴 값중 최대값으로 확정(OP는 최대한 많은 걸음을 택할 것이기 때문)
+        //      =OPP는 P의 의해 무조건 지게 될 것이고 lc에 이 값을 추가
+        //3. 그 외, P가 지는 경우가 하나라도 있다면 OP에 의해서 무조건 지게 됨.
+        //      (따라서 lc중 상대방에 의해서 최소가 택해질 것.)
+        //      =OPP는 이겼으므로 그 값을 wc에 추가
+
+
+        //일단 OPP의 이긴 유무와 받을 값만 구함
+        Frame& p=GetP();
+        bool OPPwin=false;
+        int v=0;
+        if(p.wc.empty() && p.lc.empty()){
+           OPPwin=true;
+           v=st[0].size()+st[1].size()-2;
+        }
+        else if(p.lc.empty()){
+            v=*max_element(p.wc.begin(),p.wc.end());    
+        }
+        else{
+            OPPwin=true;
+            v=*min_element(p.lc.begin(),p.lc.end());
+        }
+
+        //OP제거
+        if(st[0].size()==st[1].size()){
+            st[1].pop();
+        }
+        else{
+            st[0].pop();
+        }
+
+        //Break Point 검사
+        if(st[0].size()+st[1].size()==1){
+            ans=v;
+            return true;
+        }
+        else{
+            //OP제거 이후의 OPP는 이 때의 차례, 즉 P가 됨
+                Frame& opp=GetP();
+                if(OPPwin){
+                    opp.wc.push_back(v);
+                }
+                else{
+                    opp.lc.push_back(v);
+                }
+                return false;
+        }
+    }
+
+    void Backtrack(){
+        //Update에서 이미 팝이 되었음. 따라서 발판은 현재의 P에서 채워줘야 함
+        Frame& p=GetP();
+        board[p.loc.x][p.loc.y]=1;
+    }
+
+    void Proceed(){
+        Frame& p=GetP();
+        if(board[p.loc.x][p.loc.y]==0){
+            p.nd=4;
+            return;
+        }
+
+        int nx=p.loc.x+dir[p.nd].first;
+        int ny=p.loc.y+dir[p.nd].second;
+        if(nx<0||ny<0||board.size()<=nx||board[0].size()<=ny||board[nx][ny]==0){
+            ++p.nd;
+            return;
+        }
+        
+        board[p.loc.x][p.loc.y]=0;
+        stack<Frame> &s=GetS();
+        s.push(Frame(Loc(nx,ny)));
+        GetP().nd=0;
+    }
+
+    void GetAns(){
+        while(true){
+            //P의 차례. P는 안가본 방향 중 갈 수 있는 방향을 탐색한다.
+            //    - 갈 방향이 없다면
+            //        - 가장 첫 A의 스택이라면 모든 경우를 봤으므로, 해당 프레임 기반의 ans를 리턴하고 종료 
+            //        - 그게 아니면 (일반적) Update->Backtrack
+            //    - 갈 방향이 있다면 Proceed
+
+            Frame& p=GetP();
+            if(p.nd==4){
+                if(Update()){
+                    break;
+                }
+                Backtrack();
+            }
+            else{
+                Proceed();
+            }
+        }
+    }
+
+public:
+    int ans;
+    
+    Solver(vector<vector<int>> _board, vector<int> _aloc, vector<int> _bloc):board(_board){
+        st[0].push(Frame(Loc(_aloc[0],_aloc[1])));
+        st[0].push(Frame(Loc(_bloc[0],_bloc[1])));
+
+        GetAns();
+    }
+};
+
+int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) {
+    Solver sol(board,aloc,bloc);
+    return sol.ans;
 }
